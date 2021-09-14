908ac429a35742b6828363de4a61f9e80c65dcf0
Larry Clement <lclement@calbaptist.edu>
2021-09-14 11:01:28 -0700

    Initial Commit

---
+++ src/A_HelloWorld.java
@@ -0,0 +1,7 @@
+import java.awt.desktop.SystemEventListener;
+
+public class A_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/B_HelloWorld2.java
@@ -0,0 +1,5 @@
+public class B_HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Everyone in the Universe!");
+    }
+}
